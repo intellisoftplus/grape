82564f9d6842959e367d7b784d464aef93e30a57
John Karanja <arakanj@gmail.com>
2016-09-09 09:15:14 +0300

    added navigation draw

--- app/src/main/java/com/intellisoftplus/grape/DashboardActivity.java
+++ app/src/main/java/com/intellisoftplus/grape/DashboardActivity.java
@@ -1,8 +1,12 @@
 package com.intellisoftplus.grape;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +16,9 @@ import com.facebook.login.LoginManager;
 import java.util.HashMap;
 
 public class DashboardActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    ActionBarDrawerToggle toggle;
 
     // User Session Manager Class
     UserSessionManager session;
@@ -21,11 +28,22 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        toolbar= (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         // Session class instance
         session = new UserSessionManager(getApplicationContext());
 
         if(session.checkLogin())
             finish();
+
 
         // get user data from session
         HashMap<String, String> user = session.getUserDetails();
@@ -52,6 +70,14 @@ public class DashboardActivity extends AppCompatActivity {
         Button logout = (Button)findViewById(R.id.bLogout);
         logout.setOnClickListener(clickHandler);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(toggle.onOptionsItemSelected(item))
+            return  true;
+        return super.onOptionsItemSelected(item);
     }
 
     View.OnClickListener clickHandler = new View.OnClickListener(){
