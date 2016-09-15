2f87ce67db0b34a893898b16ff8c19daac92685d
John Karanja <arakanj@gmail.com>
2016-09-15 18:09:24 +0300

    added recyclerview renewal list test files

--- app/src/main/java/com/intellisoftplus/grape/adapters/RenewalAdapter.java
+++ app/src/main/java/com/intellisoftplus/grape/adapters/RenewalAdapter.java
@@ -1,7 +1,63 @@
 package com.intellisoftplus.grape.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.intellisoftplus.grape.R;
+import com.intellisoftplus.grape.model.ListItem;
+
+import java.util.List;
+
 /**
  * Created by user on 9/13/2016.
  */
-public class RenewalAdapter {
+public class RenewalAdapter extends RecyclerView.Adapter <RenewalAdapter.RenewalHolder> {
+
+    private List<ListItem> listData;
+    private LayoutInflater inflater;
+
+    public RenewalAdapter (List<ListItem> listData, Context c) {
+        this.inflater = LayoutInflater.from(c);
+        this.listData = listData;
+    }
+
+    @Override
+    public RenewalHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_items, parent, false);
+        return new RenewalHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RenewalHolder holder, int position) {
+        ListItem item = listData.get(position);
+        holder.title.setText(item.getTitle());
+        holder.icon.setImageResource(item.getImageResId());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    class RenewalHolder extends RecyclerView.ViewHolder {
+
+        private TextView title;
+        private ImageView icon;
+        private View container;
+
+        public RenewalHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView)itemView.findViewById(R.id.lbl_item_text);
+            icon = (ImageView)itemView.findViewById(R.id.item_icon);
+            container = itemView.findViewById(R.id.cont_item_root);
+
+        }
+    }
 }
--- app/src/main/java/com/intellisoftplus/grape/ui/RenewalsList.java
+++ app/src/main/java/com/intellisoftplus/grape/ui/RenewalsList.java
@@ -2,14 +2,27 @@ package com.intellisoftplus.grape.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.intellisoftplus.grape.R;
+import com.intellisoftplus.grape.adapters.RenewalAdapter;
+import com.intellisoftplus.grape.model.RenewalData;
 
 public class RenewalsList extends AppCompatActivity {
+
+    private RecyclerView recView;
+    private RenewalAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_renewals_list);
+
+        recView = (RecyclerView)findViewById(R.id.rec_list);
+        recView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new RenewalAdapter(RenewalData.getListData(), this);
+        recView.setAdapter(adapter);
     }
 }
