7cceb82bb3b79836ed055c27f95b1350531a670b
John Karanja <arakanj@gmail.com>
2016-09-19 15:11:20 +0300

    changed prpcts/leads"

--- app/src/main/java/com/intellisoftplus/grape/LeadsActivity.java
+++ app/src/main/java/com/intellisoftplus/grape/LeadsActivity.java
@@ -3,6 +3,7 @@ package com.intellisoftplus.grape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.Toolbar;
 
 
 public class LeadsActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class LeadsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leads);
+
+
         FragmentManager fManager = getSupportFragmentManager();
         fManager.beginTransaction()
                 .add(R.id.leadsContainer, new LeadsListFragment())
